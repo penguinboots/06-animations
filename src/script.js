370b79e0a83bbd5b03ae38328765c83942a68f25
penguinboots <sabrina.ynw@gmail.com>
2023-09-08 11:18:38 -0700

    animation exercise

--- src/script.js
+++ src/script.js
@@ -29,3 +29,24 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+// Time
+let time = Date.now();
+
+// Animations
+const tick = () => {
+  // Time
+  const currentTime = Date.now();
+  const deltaTime = currentTime - time;
+  time = currentTime;
+
+  // Update objects
+  mesh.rotation.y += 0.001 * deltaTime;
+
+  // Render
+  renderer.render(scene, camera)
+
+  window.requestAnimationFrame(tick)
+}
+
+tick();
